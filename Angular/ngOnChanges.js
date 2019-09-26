498d85d9cd0a59142c3de35708e2093df2f09a59
Synne Fog <sfog@patentstyret.no>
2019-09-26 10:39:41 +0200

    Add ngOnChanges

---
+++ Angular/ngOnChanges.js
@@ -0,0 +1,12 @@
+
+
+in onInit/the constructor add this.onChanges()
+
+ngOnChanges(){
+    this.form.get('value').valueChanges().subscribe(value => {
+        do something
+    }) 
+}
+
+// This will do somehing every time the value of 'value' onChanges
+// This way, we don't get spammed with outputs whenever any input in the component changes
