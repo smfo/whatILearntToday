a4078797802741b9e04880cf2e8bd43ae3bf3702
Synne Fog <sfog@patentstyret.no>
2019-10-08 14:49:52 +0200

    Add location

---
+++ location.js
@@ -0,0 +1,11 @@
+
+the location object
+
+protocol: (http:/https:)
+host: www.nrk.no / localhost:60000
+hostname: nrk / localhost
+href: https://nrk.no/   http://localhost:60000/#/about           //complete link
+origin: https://nrk.no/   http://localhost:60000                 //base URL
+pathname: /advanced/                                             //for search.patentstyret.no/advanced/#/
+port: 60000                                                      //for localhost only?
+hash: #/about                                                    //what follows the hash (href - (origin - pathname))
